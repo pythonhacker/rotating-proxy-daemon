9da83a11c8e62a95af14f5dad62d17ed1aa1b718
Anand B Pillai <anand@synup.com>
2016-08-25 14:56:42 +0530

    Changes in fabfile

--- fabfile.py
+++ fabfile.py
@@ -9,7 +9,7 @@ def process_proxy_host():
 
     with settings(warn_only=True):
         run("sudo iptables-restore < /etc/iptables.rules")
-        run("pgrep -f squid3; if [ $? -eq 1 ]; then sudo squid3 -YC -f /etc/squid3/squid.conf; fi")
+        run("pgrep -f squid3; if [ $? -eq 1 ]; then sudo squid3 -f /etc/squid3/squid.conf; fi")
         
 def iptables_apply():
     """ Apply iptables rules from /etc/iptables.rules """
