93ce1dcb78ba6f0c85ca6f7d82f15722f66cea7d
Anand B Pillai <abp@tingtun.no>
2017-04-03 12:51:06 +0530

    Use gmail to send email

--- email_report.py
+++ email_report.py
@@ -4,7 +4,7 @@ import sys
 import socket
 import os
 import ses_email
-
+import send_gmail
 
 def email_report(config, template, content):
     """ Email any kind of report to anyone """
@@ -17,8 +17,10 @@ def email_report(config, template, content):
     if config.get('send_email', True):
         print 'Sending email ...'
         from_e, to_e = config.get('from_email'), config.get('to_email')
+        from_pass = config.get('from_pass')
         subject = config.get('email_subject') % (timestamp, socket.gethostname())
-        print ses_email.send_ses(from_e, subject, data, to_e)
+        print send_gmail.send_mail(from_e, from_pass, to_e, subject, data)
+        # print ses_email.send_ses(from_e, subject, data, to_e)
         print 'done.'
     else:
         print 'Not sending email.'
