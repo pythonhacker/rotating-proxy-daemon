e418f90c3724307ae79ec969f447bff2c799b936
Anand B Pillai <anandpillai@letterboxes.org>
2019-10-02 12:23:54 +0530

    Removing all hard-coded linode references

--- rotate_proxies.py
+++ rotate_proxies.py
@@ -1,11 +1,13 @@
 """
-Script to auto-rotate and configure Linodes as squid proxies.
+Script to auto-rotate and configure squid proxy farms using multiple
+VPS backends using their APIs.
 
 """
 
 import argparse
 import os
 import sys
+import signal
 
 def process_args(rotator, args):
 
@@ -15,17 +17,17 @@ def process_args(rotator, args):
         sys.exit(0)
         
     if args.add != 0:
-        print 'Adding new set of',args.num,'linode proxies ...'
+        print 'Adding new set of',args.num,'proxies ...'
         rotator.provision(count = int(args.num), add=True)
         sys.exit(0)
         
     if args.provision != 0:
-        print 'Provisioning fresh set of',args.num,'linode proxies ...'
+        print 'Provisioning fresh set of',args.num,'proxies ...'
         rotator.provision(count = int(args.num))
         sys.exit(0)
         
     if args.create:
-        print 'Creating new linode...'
+        print 'Creating new instance...'
         rotator.create(int(args.region))
         sys.exit(0)
 
@@ -70,21 +72,21 @@ if __name__ == "__main__":
     parser.add_argument('-C','--conf',help='Use the given configuration file', default='proxy.conf')    
     parser.add_argument('-s','--stop',help='Stop the currently running daemon', action='store_true')
     parser.add_argument('-t','--test',help='Run the test function to test the daemon', action='store_true')
-    parser.add_argument('-c','--create',help='Create a proxy linode', action='store_true',default=False)
-    parser.add_argument('-r','--region',help='Specify a region when creating a linode', default=3, type=int)
+    parser.add_argument('-c','--create',help='Create a proxy instance', action='store_true',default=False)
+    parser.add_argument('-r','--region',help='Specify a region when creating an instance', default=3, type=int)
     parser.add_argument('-R','--rotate',help='Rotate a node immediately and go to sleep', default=False,
                         action='store_true')
     parser.add_argument('-D','--drop',help='Drop the current configuration of proxies (except LB)',
                         default=False,action='store_true')
-    parser.add_argument('-P','--provision',help='Provision a fresh set of proxy linodes',default=False,
+    parser.add_argument('-P','--provision',help='Provision a fresh set of proxy instances',default=False,
                         action='store_true')
-    parser.add_argument('-A','--add',help='Add a new set of linodes to existing set',default=False,
+    parser.add_argument('-A','--add',help='Add a new set of instances to existing farm',default=False,
                         action='store_true')    
-    parser.add_argument('-N','--num',help='Number of new linodes to provision or add (use with -P or -A)',type=int,
+    parser.add_argument('-N','--num',help='Number of new instances to provision or add (use with -P or -A)',type=int,
                         default=10)
     
-    parser.add_argument('-w','--writeconfig',help='Load current Linode proxies configuration and write a fresh proxies.list config file', action='store_true')
-    parser.add_argument('-W','--writelbconfig',help='Load current Linode proxies configuration and write a fresh HAProxy config to /etc/haproxy/haproxy.cfg', action='store_true')
+    parser.add_argument('-w','--writeconfig',help='Load current proxies configuration and write a fresh proxies.list config file', action='store_true')
+    parser.add_argument('-W','--writelbconfig',help='Load current proxies configuration and write a fresh HAProxy config to /etc/haproxy/haproxy.cfg', action='store_true')
     parser.add_argument('--restart',help='Restart the daemon',action='store_true')
     parser.add_argument('-T','--target',help='Target VPS platform (linode, aws)',default='linode')
         
@@ -96,7 +98,7 @@ if __name__ == "__main__":
         rotator = linode.LinodeProxyRotator(cfg=args.conf,
                                             test_mode = args.test,
                                             rotate=args.rotate)
-    else:
+    elif args.target == 'aws':
         aws = __import__('aws')
         rotator = aws.AwsProxyRotator(cfg=args.conf,
                                       test_mode = args.test,
